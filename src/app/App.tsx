83c7a1224fb0d0a28ac943edc3001d9104f02b23
michaelcjefferson <michaelcjefferson@gmail.com>
2026-04-07 17:18:58 +1200

    Refactor: start scaffolding project structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
@@ -11,9 +11,9 @@ function App() {
     <>
       <section id="center">
         <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          {/* <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" /> */}
         </div>
         <div>
           <h1>Get started</h1>
@@ -41,13 +41,13 @@ function App() {
           <ul>
             <li>
               <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
+                {/* <img className="logo" src={viteLogo} alt="" /> */}
                 Explore Vite
               </a>
             </li>
             <li>
               <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
+                {/* <img className="button-icon" src={reactLogo} alt="" /> */}
                 Learn more
               </a>
             </li>
