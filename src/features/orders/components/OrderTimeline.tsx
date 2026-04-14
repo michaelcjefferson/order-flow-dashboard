c1d1472049eb466dd1026e97c69922ceab4accb0
michaelcjefferson <michaelcjefferson@gmail.com>
2026-04-14 21:46:58 +1200

    Style: different colours/border styles to indicate whether order status is completed, in progress, or not started

--- src/features/orders/components/OrderTimeline.tsx
+++ src/features/orders/components/OrderTimeline.tsx
@@ -19,26 +19,40 @@ const ORDER_STATUS_FLOW: OrderStatus[] = [
 ];
 
 function OrderTimeline({ history, fromAddress, toAddress }: Props) {
+  const completedStatuses = history.map(h => h.status);
+
   return (
     <div className="flex w-full items-center justify-between">
       <p className="font-bold">{fromAddress}</p>
-      {history.map(h => (
-        // TODO: better to pass order id?
-        <div key={h.status} className={
-          clsx("flex flex-col justify-center items-center px-4 py-4 w-24 h-24",
-            {
-              "bg-amber-100 text-amber-800 border border-amber-400 shadow-sm shadow-amber-700/50": h.status === OrderStatus.Pending,
-              // "bg-amber-100 text-amber-800 border border-amber-400 inset-shadow-sm inset-shadow-amber-700/50": h.status === OrderStatus.Pending,
-              "bg-blue-100 text-blue-800 border border-blue-400 shadow-sm shadow-blue-700/50": h.status === OrderStatus.Processing,
-              "bg-purple-100 text-purple-800 border border-purple-400 shadow-sm shadow-purple-700/50": h.status === OrderStatus.Shipped,
-              "bg-green-100 text-green-800 border border-green-400 shadow-sm shadow-green-700/50": h.status === OrderStatus.Delivered,
-              "bg-red-100 text-red-800 border border-red-400 shadow-sm shadow-red-700/50": h.status === OrderStatus.Cancelled,
-            }
-          )}>
-          <p className="text-m font-medium capitalize">{h.status}</p>
-          <p className="text-s font-light">{formatShortDate(h.startedAt)}</p>
-        </div>
-      ))}
+      {ORDER_STATUS_FLOW.map((status, index) => {
+        const isCompleted = completedStatuses.includes(status);
+        const isLast = index === ORDER_STATUS_FLOW.length - 1;
+        const isCurrent = index === history.length - 1;
+
+        const entry = history.find(h => h.status === status);
+
+        return (
+          <div key={status} className={
+            clsx("flex flex-col justify-center items-center px-4 py-4 w-24 h-24 border",
+              {
+                "bg-amber-100 text-amber-800 border-amber-400 shadow-sm shadow-amber-700/50": isCompleted && status === OrderStatus.Pending,
+                // "bg-amber-100 text-amber-800 border border-amber-400 inset-shadow-sm inset-shadow-amber-700/50": h.status === OrderStatus.Pending,
+                "bg-blue-100 text-blue-800 border-blue-400 shadow-sm shadow-blue-700/50": isCompleted && status === OrderStatus.Processing,
+                "bg-purple-100 text-purple-800 border-purple-400 shadow-sm shadow-purple-700/50": isCompleted && status === OrderStatus.Shipped,
+                "bg-green-100 text-green-800 border-green-400 shadow-sm shadow-green-700/50": isCompleted && status === OrderStatus.Delivered,
+                "bg-red-100 text-red-800 border-red-400 shadow-sm shadow-red-700/50": status === OrderStatus.Cancelled,
+                "bg-white border-2 border-cyan-700/50 rounded-sm border-dotted": !isCompleted,
+                "rounded-full border-solid w-25 h-25": isCompleted && !isCurrent,
+                "rounded-4xl border-dashed": isCurrent && !isLast,
+              }
+            )}>
+            <p className={clsx("text-m font-medium capitalize", {
+              "text-black": !isCompleted,
+            })}>{status}</p>
+            <p className="text-s font-light">{formatShortDate(entry?.startedAt)}</p>
+          </div>
+        )
+      })}
       <p className="font-bold">{toAddress}</p>
     </div>
   )
